4b388831854e1a0f8aeb715e7e56e83c633dfae4
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-01-20 14:11:11 +0900

    Add teapot to the test scenes.

---
+++ Assets/Test/SimpleRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    [SerializeField] float _spinSpeed = 10;
+    [SerializeField] float _pitchSpeed = 1;
+    [SerializeField] float _pitchAmount = 10;
+
+    void Update()
+    {
+        var rx = Mathf.Sin(Time.time * _pitchSpeed) * _pitchAmount;
+        transform.localRotation =
+            Quaternion.Euler(rx, _spinSpeed * Time.time, 0);
+    }
+}
